e4ecaa1190cc82c30ca1933a6d2253d1661475be
SvetlanaBuyanovskaya <svetabuyanovska@gmail.com>
2016-08-06 19:32:12 +0300

    replace project Fragment

--- Factorial/app/src/main/java/com/fandroid/factorial/Factorial.java
+++ Factorial/app/src/main/java/com/fandroid/factorial/Factorial.java
@@ -2,16 +2,13 @@ package com.fandroid.factorial;
 
 public class Factorial {
     public static void main(String args[]) {
-        DecimalBigInt fact = new DecimalBigInt(1);
-        for (int i = 1; i <= 100; i++) {
-            fact = fact.times(new DecimalBigInt(i));
-        }
+        DecimalBigInt fac=DecimalBigInt.factorial(100);
         int sum = 0;
-        String s = fact.toDecimalString();
+        String s = fac.toDecimalString();
         for (int i = 0; i < s.length(); i++) {
             sum = sum + Integer.parseInt("" + s.charAt(i));
         }
-        System.out.println("FAC (100) =" + fact.toDecimalString() + ""
-                + "\n" + "Summ of te digist:" + sum);
+        System.out.println("factorial (100) =" + fac.toDecimalString()
+                + "\n" + "Summ of te digist:" + s);
     }
 }
